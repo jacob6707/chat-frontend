3dc436fea3820618c02090a0c57b4a9d318750fd
jacob6707 <22101656+jacob6707@users.noreply.github.com>
2024-04-20 22:14:52 +0200

    Fix message button not working in user profile

--- src/features/user/UserProfile.jsx
+++ src/features/user/UserProfile.jsx
@@ -3,15 +3,19 @@ import AvatarImage from "../../components/AvatarImage";
 import Spinner from "../../ui/Spinner";
 import { useUser } from "../authentication/useUser";
 import { useAddFriend } from "../friends/useAddFriend";
+import { useMessageFriend } from "../friends/useMessageFriend";
 import { useRemoveFriend } from "../friends/useRemoveFriend";
 import StatusBlip from "./StatusBlip";
 import { useUserById } from "./useUserById";
 
-function UserProfile({ id }) {
+function UserProfile({ id, onCloseModal }) {
   const { user, isLoading, error } = useUserById(id);
   const { user: currentUser, isLoading: isLoadingCurrentUser } = useUser();
-  const { addFriend } = useAddFriend();
-  const { removeFriend } = useRemoveFriend();
+  const { addFriend, isAddingFriend } = useAddFriend();
+  const { removeFriend, isRemovingFriend } = useRemoveFriend();
+  const { messageFriend, isMessagingFriend } = useMessageFriend();
+
+  const isWorking = isAddingFriend || isRemovingFriend || isMessagingFriend;
 
   if (isLoading || isLoadingCurrentUser) return <Spinner />;
 
@@ -40,15 +44,23 @@ function UserProfile({ id }) {
           <StatusBlip status={user.status.current} size="large" />
         </div>
         <div>
-          <h1 className="text-2xl">
-            {user.displayName}{" "}
-            <span className="text-lg text-slate-400">({user.username})</span>
-          </h1>
+          <h1 className="text-2xl">{user.displayName} </h1>
+          <span className="text-base text-slate-400">({user.username})</span>
           <p className="text-slate-400">{user.status.current}</p>
         </div>
         <div className="ml-auto flex items-center gap-2">
           {friends && friends === 3 && (
-            <button className="rounded-lg bg-green-500 px-4 py-2 text-white hover:bg-green-600">
+            <button
+              className="rounded-lg bg-green-500 px-4 py-2 text-white hover:bg-green-600 disabled:bg-green-800/50 disabled:text-white/50"
+              onClick={() =>
+                messageFriend(id, {
+                  onSettled: () => {
+                    onCloseModal?.();
+                  },
+                })
+              }
+              disabled={isWorking}
+            >
               Message
             </button>
           )}
@@ -56,23 +68,26 @@ function UserProfile({ id }) {
             (friends ? (
               friends === 3 || friends === 1 ? (
                 <button
-                  className="rounded-lg bg-red-500 px-4 py-2 text-white hover:bg-red-600"
+                  className="rounded-lg bg-red-500 px-4 py-2 text-white hover:bg-red-600 disabled:bg-red-800/50 disabled:text-white/50"
                   onClick={() => removeFriend(id)}
+                  disabled={isWorking}
                 >
                   {friendButtonText[friends]}
                 </button>
               ) : (
                 <button
-                  className="rounded-lg bg-green-500 px-4 py-2 text-white hover:bg-green-600"
+                  className="rounded-lg bg-green-500 px-4 py-2 text-white hover:bg-green-600 disabled:bg-green-800/50 disabled:text-white/50"
                   onClick={() => addFriend(id)}
+                  disabled={isWorking}
                 >
                   {friendButtonText[friends]}
                 </button>
               )
             ) : (
               <button
-                className="rounded-lg bg-green-500 px-4 py-2 text-white hover:bg-green-600"
+                className="rounded-lg bg-green-500 px-4 py-2 text-white hover:bg-green-600 disabled:bg-green-800/50 disabled:text-white/50"
                 onClick={() => addFriend(id)}
+                disabled={isWorking}
               >
                 Add friend
               </button>
